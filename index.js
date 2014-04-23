df4b015d599df864794eb35a3d360cca6c7e3802
yangjie <hiyoungjay@gmail.com>
2014-04-23 16:59:46 +0800

    add instance creator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mixin = require('mixin-class');
+var _ = require('lodash');
 
 module.exports = function(factories) {
     var Classes = {};
@@ -10,16 +11,61 @@ module.exports = function(factories) {
             throw new Error('duplicated component with path: "' + exports.path + '"');
         }
         Classes[exports.path] = mixin(exports.vm, {
-            __view: exports.view
+            __view: exports.view,
+            __path: exports.path
         })
         // TODO
         // insert style to document
     });
 
-    return function(path) {
+    var getClass = function(path) {
         if (!Classes[path]) {
             throw new Error('component with path: "' + path +  '" not exists');
         }
         return Classes[path];
+    };
+
+    var getInstance = function(config) {
+        var instance = getClass(config.type)();
+        if (config.data) {
+            if (instance.setData) {
+                instance.setData(config.data);
+            }
+        }
+
+        if (config.mixin) {
+            _.extend(instance, config.mixin);
+        }
+
+        if (config.route) {
+            instance.__route = config.route;
+        }
+
+        if (config.component) {
+            _.forEach(config.component, function(subConfig, key) {
+                instance[key] = getInstanceOrs(subConfig);
+            })
+        }
+
+        return instance;
+    };
+
+    var getInstances = function(configs) {
+        return configs.map(getInstance);
+    };
+
+    var getInstanceOrs = function(config) {
+        if (Array.isArray(config)) {
+            return getInstances(config);
+        }
+
+        return getInstance(config);
+    };
+
+    return function(pathOrConfig) {
+        if (typeof pathOrConfig === 'string') {
+            return getClass(pathOrConfig);
+        }
+        return getInstanceOrs(pathOrConfig);
     }
 };
--- test/index.js
+++ test/index.js
@@ -26,3 +26,30 @@ describe('component factory', function() {
 
     })
 });
+
+describe('component create instance', function() {
+    var Component = ComponentFactory(factories);
+
+    it('should create instance', function() {
+        var c = Component({
+            type: 'index',
+            mixin: {
+                a: 1
+            },
+            component: {
+                one: {
+                    type: 'index'
+                },
+                two: [{type:'index'},{type:'shop/header'}]
+            }
+        });
+
+        assert.equal(c.__path, 'index')
+        assert.equal(c.a, 1)
+        assert.equal(c.one.__path, 'index')
+        assert.equal(c.two.length, 2)
+        assert.equal(c.two[0].__path, 'index')
+        assert.equal(c.two[1].__path, 'shop/header')
+    })
+
+})
