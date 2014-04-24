68ab16c02a88bb631e1a8795c60f9b1c87fbcaf9
yangjie <hiyoungjay@gmail.com>
2014-04-24 10:26:58 +0800

    simplified

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var mixin = require('mixin-class');
-var _ = require('lodash');
 
 module.exports = function(factories) {
     var Classes = {};
@@ -8,64 +7,19 @@ module.exports = function(factories) {
         var exports = {};
         factory(exports);
         if (Classes[exports.path]) {
-            throw new Error('duplicated component with path: "' + exports.path + '"');
+            throw new Error('duplicated component for path: "' + exports.path + '"');
         }
         Classes[exports.path] = mixin(exports.vm, {
-            __view: exports.view,
-            __path: exports.path
+            __view: exports.view
         })
         // TODO
         // insert style to document
     });
 
-    var getClass = function(path) {
+    return function(path) {
         if (!Classes[path]) {
             throw new Error('component with path: "' + path +  '" not exists');
         }
         return Classes[path];
-    };
-
-    var getInstance = function(config) {
-        var instance = getClass(config.type)();
-        if (config.data) {
-            if (instance.setData) {
-                instance.setData(config.data);
-            }
-        }
-
-        if (config.mixin) {
-            _.extend(instance, config.mixin);
-        }
-
-        if (config.route) {
-            instance.__route = config.route;
-        }
-
-        if (config.component) {
-            _.forEach(config.component, function(subConfig, key) {
-                instance[key] = getInstanceOrs(subConfig);
-            })
-        }
-
-        return instance;
-    };
-
-    var getInstances = function(configs) {
-        return configs.map(getInstance);
-    };
-
-    var getInstanceOrs = function(config) {
-        if (Array.isArray(config)) {
-            return getInstances(config);
-        }
-
-        return getInstance(config);
-    };
-
-    return function(pathOrConfig) {
-        if (typeof pathOrConfig === 'string') {
-            return getClass(pathOrConfig);
-        }
-        return getInstanceOrs(pathOrConfig);
     }
 };
--- test/index.js
+++ test/index.js
@@ -7,49 +7,22 @@ describe('component factory', function() {
 
     it('should throw Error when duplicated path are provide', function() {
         assert.throws(function() {
-            var Component = ComponentFactory(factories.concat(factories));
+            var componentFactory = ComponentFactory(factories.concat(factories));
         }, Error)
     })
 
     it('should throw error when component is not provide', function() {
-        var Component = ComponentFactory(factories);        
+        var componentFactory = ComponentFactory(factories);        
         assert.throws(function() {
-            Component('not-exists-path');
+            componentFactory('not-exists-path');
         }, Error)
     });
 
     it('should return mixined component with __view', function() {
-        var Component = ComponentFactory(factories);
-        var c = Component('index')();
+        var componentFactory = ComponentFactory(factories);
+        var c = new (componentFactory('index'))();
 
         assert.equal(c.__view, "<div class=\"name\">\n    hello\n</div>")
 
     })
 });
-
-describe('component create instance', function() {
-    var Component = ComponentFactory(factories);
-
-    it('should create instance', function() {
-        var c = Component({
-            type: 'index',
-            mixin: {
-                a: 1
-            },
-            component: {
-                one: {
-                    type: 'index'
-                },
-                two: [{type:'index'},{type:'shop/header'}]
-            }
-        });
-
-        assert.equal(c.__path, 'index')
-        assert.equal(c.a, 1)
-        assert.equal(c.one.__path, 'index')
-        assert.equal(c.two.length, 2)
-        assert.equal(c.two[0].__path, 'index')
-        assert.equal(c.two[1].__path, 'shop/header')
-    })
-
-})
